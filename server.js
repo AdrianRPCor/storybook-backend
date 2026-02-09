a3fad64d549e51259b6630fbc57531b4234a9039
AdrianRPCor <proyectoarenaeducacion@gmail.com>
2026-02-09 23:58:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ dotenv.config();
 
 console.log("🔑 OPENAI_API_KEY =", process.env.OPENAI_API_KEY);
 
-console.log("🔑 OPENAI_API_KEY presente:", !!process.env.OPENAI_API_KEY);
 
 /* =========================
    APP
